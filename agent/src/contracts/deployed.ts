e7cee3c81159e989cc0ca8cc7c87086fbe7fddfd
Mathijs Vogelzang <mathijs@vogelzang.xyz>
2026-02-08 00:06:26 +0100

    contracts: deployed to unichain

--- agent/src/contracts/deployed.ts
+++ agent/src/contracts/deployed.ts
@@ -4,4 +4,8 @@ export const deployedContracts = {
   "manager": "0xF59E926DA58fb89F5676a7BF5F00a37D8bEbA111",
   "vault": "0xCE4a121e7aB0790C76d56229f2baD0ef909Be85a"
 },
+  130: {
+  "hook": "0x3002a90B7C510d33debeE13e46854711f45a50C4",
+  "manager": "0xce346088CC4354aDBb4cBcC41533bed1A0568D4e"
+},
 }
